0e0de885b59528052aa291542bd030ec07e3d76a
Oriol <0m1c20n@gmail.com>
2019-01-11 18:07:21 +0100

    Some adjustments and finished report

--- cpe.R
+++ cpe.R
@@ -2,41 +2,37 @@
 #                                                                              #
 #                          Lab 2 - CPE Standard                                #
 #                                                                              #
-#              Arnau Sangra Rocamora - Data Driven Securty                     #
+#              Oriol Mominó Villegas - Data Driven Securty                     #
 #                                                                              #
 #******************************************************************************#
+DownloadCPEData <- function() {
+  compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
+  cpes_filename <- "cpes.zip"
+  if(!file.exists(cpes_filename)){
+    download.file(compressed_cpes_url, cpes_filename)
+  }
+  unziped_file <- "official-cpe-dictionary_v2.3.xml"
+  if(!file.exists(unziped_file)){
+    unzip(zipfile = cpes_filename)
+  }
+  cpe.file <- "./official-cpe-dictionary_v2.3.xml"
+  return(cpe.file)
+}
 
-if (!require("xml2")) install.packages("xml2")
-if (!require("stringr")) install.packages("stringr")
-if (!require("tidyr")) install.packages("tidyr")
-if (!require("dplyr")) install.packages("dplyr")
-library(xml2)
-library(stringr)
-library(tidyr)
-library(dplyr)
+ReadXMLCPEFile <- function(cpe.file) {
+  cpes.xml <- xml2::read_xml(cpe.file)
+  return(cpes.xml)
+}
 
-compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
-cpes_filename <- "cpes.zip"
-download.file(compressed_cpes_url, cpes_filename)
-unzip(zipfile = cpes_filename)
-cpe.file <- "./official-cpe-dictionary_v2.3.xml"
-
-GetCPEItems <- function(cpe.raw) {
-  #cpe <- NewCPEItem()
-  #cpe.raw <- xml2::xml_find_all(cpe.raw)
-
-  # transform the list to data frame
-  cpes <- data.frame(title = xml2::xml_text(xml2::xml_find_all(cpe.raw, "//*[cpe-23:cpe23-item]/*[@xml:lang='en-US'][1]")),
-                     cpe.23 = xml2::xml_text(xml2::xml_find_all(cpe.raw, "//*[cpe-23:cpe23-item]/*/@name")),
+GetCPEItems <- function(cpes.xml) {
+  cpes <- data.frame(title = xml2::xml_text(xml2::xml_find_all(cpes.xml, "//*[cpe-23:cpe23-item]/*[@xml:lang='en-US'][1]")),
+                     cpe.23 = xml2::xml_text(xml2::xml_find_all(cpes.xml, "//*[cpe-23:cpe23-item]/*/@name")),
                      stringsAsFactors = F)
 
-  # return data frame
   return(cpes)
 }
 
 CleanCPEs <- function(cpes){
-
-  # data manipulation
   col.names <- c("std", "std.v", "part", "vendor", "product",
                 "version", "update", "edition", "language", "sw_edition",
                 "target_sw", "target_hw", "other")
@@ -53,23 +49,5 @@ CleanCPEs <- function(cpes){
   cpes$target_sw <- as.factor(cpes$target_sw)
   cpes$target_hw <- as.factor(cpes$target_hw)
 
-  #return(data.frame())
   return(cpes)
 }
-
-ParseCPEData <- function(cpe.file) {
-
-  # load cpes as xml file
-  cpes <- xml2::read_xml(cpe.file)
-
-  # get CPEs
-  cpes <- GetCPEItems(cpes)
-
-  # transform, clean, arrange parsed cpes as data frame
-  df <- CleanCPEs(cpes)
-
-  # return data frame
-  return(df)
-}
-
-#cpes <- ParseCPEData(cpe.file)
